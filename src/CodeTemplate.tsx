b914f11c11da9b15f151bf794652d25e33a469ad
keanecjy <e0425601@u.nus.edu>
2020-12-06 13:17:25 +0800

    Add code template file

---
+++ src/CodeTemplate.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CodeTemplate() {
+  return (
+    <div className='Code'>
+      <header className='Code-header'>
+        Code template here
+      </header>
+    </div>
+  );
+}
+
+export default CodeTemplate;
