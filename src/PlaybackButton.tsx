4ef313396205bf979b002833362e09bccdf3f6e0
keanecjy <e0425601@u.nus.edu>
2020-12-06 13:34:43 +0800

    Add template file

---
+++ src/PlaybackButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PlayBackButton() {
+  return (
+    <div className='PlaybackButton'>
+      <header className='PlaybackButton-header'>
+        Code template here
+      </header>
+    </div>
+  );
+}
+
+export default PlayBackButton;
