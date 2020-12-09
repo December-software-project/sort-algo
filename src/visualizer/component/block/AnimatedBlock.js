1f50296c569c8bffcfc56312cb9e1032e0f8e31c
keanecjy <e0425601@u.nus.edu>
2020-12-09 15:56:26 +0800

    Reformat code

--- src/visualizer/component/block/AnimatedBlock.js
+++ src/visualizer/component/block/AnimatedBlock.js
@@ -3,13 +3,18 @@ import './styles.css';
 import { animated } from 'react-spring';
 
 const AnimatedBlock = ({ item, props: { x, ...rest }, length, index }) => {
-  return <animated.div
-    className='temp' style={{
-    height: item.height,
-    backgroundImage: item.css,
-    zIndex: length - index,
-    transform: x.interpolate(x => `translate3d(${x}px,0,0)`), ...rest,
-  }} />;
+  return (
+    <animated.div
+      className="temp"
+      style={{
+        height: item.height,
+        backgroundImage: item.css,
+        zIndex: length - index,
+        transform: x.interpolate((x) => `translate3d(${x}px,0,0)`),
+        ...rest,
+      }}
+    />
+  );
 };
 
 export default AnimatedBlock;
--- src/visualizer/index.js
+++ src/visualizer/index.js
@@ -1,29 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import insertionSort from './algorithm/insertionSort';
 import data from './component/block/data';
 import AnimationScreen from './sortingvisualizer/AnimationScreen';
 
 const Visualizer = () => {
-
   const [isPlay, setIsPlay] = useState(false);
-  const animationArr = insertionSort(data.map(x => x));
+  const animationArr = insertionSort(data.map((x) => x));
   const swap = (firstIdx, secondIdx, arr) => {
-    let newTempArr = arr.map(x => x);
+    let newTempArr = arr.map((x) => x);
     let tmp = newTempArr[firstIdx];
     newTempArr[firstIdx] = newTempArr[secondIdx];
     newTempArr[secondIdx] = tmp;
     return newTempArr;
   };
   useEffect(() => {}, [isPlay]);
-  
-  return (<div>
+
+  return (
     <div>
-      <AnimationScreen dataArray={data} animationArr={animationArr}
-                       swap={(firstIdx, secondIdx, arr) => swap(firstIdx,
-                         secondIdx, arr)} isPlay={isPlay}/>
+      <div>
+        <AnimationScreen
+          dataArray={data}
+          animationArr={animationArr}
+          swap={(firstIdx, secondIdx, arr) => swap(firstIdx, secondIdx, arr)}
+          isPlay={isPlay}
+        />
+      </div>
+      <button onClick={() => setIsPlay(!isPlay)}>play</button>
     </div>
-    <button onClick={() => setIsPlay(!isPlay)}>play</button>
-  </div>);
+  );
 };
 
 export default Visualizer;
