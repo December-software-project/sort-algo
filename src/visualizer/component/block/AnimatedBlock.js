6421507e502de85c95d34a02f8275541a316edc6
Sean <e0406739@u.nus.edu>
2020-12-09 21:12:19 +0800

    add play button and legend

--- src/visualizer/component/block/AnimatedBlock.js
+++ src/visualizer/component/block/AnimatedBlock.js
@@ -4,12 +4,11 @@ import { animated } from 'react-spring';
 
 const AnimatedBlock = ({ item, props: { x, ...rest }, length, index }) => {
   return <animated.div
-    className='temp' style={{
+    className='animated-block' style={{
     height: item.height,
-    backgroundImage: item.css,
     zIndex: length - index,
     transform: x.interpolate(x => `translate3d(${x}px,0,0)`), ...rest,
-  }} />;
+  }}><span>{item.height}</span></animated.div>;
 };
 
 export default AnimatedBlock;
