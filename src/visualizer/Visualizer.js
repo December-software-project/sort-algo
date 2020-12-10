d21498101ea0fe5769905c4a145fca461740822f
Sean <e0406739@u.nus.edu>
2020-12-10 18:14:20 +0800

    update Visualizer styling

--- src/visualizer/Visualizer.js
+++ src/visualizer/Visualizer.js
@@ -4,22 +4,37 @@ import data from './component/block/data';
 import AnimationScreen from './sortingvisualizer/AnimationScreen';
 import PlayBackButton from './component/playbackbutton/PlaybackButton';
 import Legend from './component/legend/Legend';
+import VisualizerHeader from './component/header/VisualizerHeader';
+import AlgorithmSelector from './component/algorithmselector/AlgorithmSelector';
 import './styles.css';
 
 const Visualizer = () => {
   const [isPlay, setIsPlay] = useState(false);
   const animationArr = insertionSort(data.map((x) => x));
   const swap = (firstIdx, secondIdx, arr) => {
-    let newTempArr = arr.map((x) => x);
+    let newTempArr = resetArray(arr);
     let tmp = newTempArr[firstIdx];
     newTempArr[firstIdx] = newTempArr[secondIdx];
     newTempArr[secondIdx] = tmp;
+    newTempArr[firstIdx].isSwap = true;
+    newTempArr[secondIdx].isSwap = true;
     return newTempArr;
+  };
+  const resetArray = (arr) => {
+    return arr.map((x) => {
+      let tempArrElement = x;
+      tempArrElement.isSwap = false;
+      return tempArrElement;
+    });
   };
   useEffect(() => {}, [isPlay]);
 
   return (
     <div className="visualizer">
+      <div className="visualizer-header-box">
+        <VisualizerHeader />
+        <AlgorithmSelector />
+      </div>
       <div className="visualizer-box">
         <AnimationScreen
           dataArray={data}
@@ -27,6 +42,7 @@ const Visualizer = () => {
           swap={(firstIdx, secondIdx, arr) => swap(firstIdx, secondIdx, arr)}
           isPlay={isPlay}
           setIsPlay={() => setIsPlay(!isPlay)}
+          resetArray={(arr) => resetArray(arr)}
         />
       </div>
       <div className="controller-box">
