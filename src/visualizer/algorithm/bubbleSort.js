7ac8394ffef0f77495cf38c4ece2eb6a59d50af6
keanecjy <e0425601@u.nus.edu>
2020-12-11 00:07:49 +0800

    Add bubble sort algo

---
+++ src/visualizer/algorithm/bubbleSort.js
@@ -0,0 +1,26 @@
+function swap(i, j, arr) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+const bubbleSort = (arr) => {
+  let receivedArr = arr;
+  let animationArr = [];
+  for (let i = receivedArr.length - 1; i >= 0; i--) {
+    let flag = true;
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        animationArr.push([j + 1, j]);
+        swap(j, j + 1, arr);
+        flag = false;
+      }
+    }
+    if (flag) {
+      break;
+    }
+  }
+  return animationArr;
+}
+
+export default bubbleSort;
