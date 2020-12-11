20ad841acf8e60e6f1938c9a349e54bac90cc3f9
Sean <e0406739@u.nus.edu>
2020-12-11 21:53:15 +0800

    fix fail build in heroku

--- src/visualizer/codeinformation/explanations/radixSortEx.js
+++ src/visualizer/codeinformation/explanations/radixSortEx.js
@@ -1,1 +1,28 @@
 // todo radix sort explanation
+const RadixSort = {
+  name: 'Quick',
+  description:
+    'QuickSort is a Divide and Conquer algorithm. It picks an element as pivot and partitions' +
+    ' the given array around the picked pivot. There are many different versions of quickSort that ' +
+    'pick pivot in different ways.',
+  worstTime: (
+    <span>
+      N<sup>2</sup>
+    </span>
+  ),
+  averageTime: (
+    <span>
+      Log<sub>2</sub>N
+    </span>
+  ),
+  bestTime: 'N',
+  worstSpace: (
+    <span>
+      Log<sub>2</sub>N
+    </span>
+  ),
+  stable: false,
+  inPlace: true,
+};
+
+export default RadixSort;
