080f7855cc75015a70df737cf36c6aab0beaede1
keanecjy <e0425601@u.nus.edu>
2020-12-11 18:20:50 +0800

    Add code template

--- src/visualizer/templates/insertionSortTemplate.js
+++ src/visualizer/templates/insertionSortTemplate.js
@@ -1,73 +1,54 @@
 const InsertionSort = {
   Java:
-    '        for (int i = arr.length - 1; i >= 0; i--) {\n' +
-    '            boolean bool = true;\n' +
-    '            for (int j = 0; j < i; j++) {\n' +
-    '                if (arr[j] > arr[j + 1]) {\n' +
-    '                    swap(j, j + 1, arr);\n' +
-    '                    bool = false;\n' +
-    '                }\n' +
-    '            }\n' +
-    '            if (bool) {\n' +
-    '                break;\n' +
-    '            }\n' +
-    '        }',
+    'for (int i = arr.length - 1; i >= 0; i--) {\n' +
+    '    boolean swapped = true;\n' +
+    '    for (int j = 0; j < i; j++) {\n' +
+    '        if (arr[j] > arr[j + 1]) {\n' +
+    '            swap(j, j + 1, arr);\n' +
+    '            swapped = false;\n' +
+    '        }\n' +
+    '    }\n' +
+    '    if (swapped) {\n' +
+    '        break;\n' +
+    '    }\n' +
+    '}',
   JavaScript:
-    '  for (let i = arr.length - 1; i >= 0; i--) {\n' +
-    '    let flag = true;\n' +
-    '    for (let j = 0; j < i; j++) {\n' +
-    '      if (arr[j] > arr[j + 1]) {\n' +
-    '        swap(j, j + 1, arr);\n' +
-    '        flag = false;\n' +
-    '      }\n' +
+    'for (let i = arr.length - 1; i >= 0; i--) {\n' +
+    '  let swapped = true;\n' +
+    '  for (let j = 0; j < i; j++) {\n' +
+    '    if (arr[j] > arr[j + 1]) {\n' +
+    '      swap(j, j + 1, arr);\n' +
+    '      swapped = false;\n' +
     '    }\n' +
-    '    if (flag) {\n' +
-    '      break;\n' +
-    '    }\n' +
-    '  }',
+    '  }\n' +
+    '  if (swapped) {\n' +
+    '    break;\n' +
+    '  }\n' +
+    '}',
   Python:
     'for i in range(len(arr) - 1, 0, -1):\n' +
-    '    flag = True\n' +
+    '    swapped = True\n' +
     '    for j in range(0, i):\n' +
     '        if arr[j] > arr[j + 1]:\n' +
     '            swap(j, j + 1, arr)\n' +
-    '            flag = False\n' +
-    '    if flag:\n' +
+    '            swapped = False\n' +
+    '    if swapped:\n' +
     '        break',
-  'C++':
-    '  for (int i = length - 1; i >= 0; i--)\n' +
+  'C/C++':
+    'for (int i = length - 1; i >= 0; i--)\n' +
+    '{\n' +
+    '  bool swapped = true;\n' +
+    '  for (int j = 0; j < i; j++)\n' +
     '  {\n' +
-    '    bool flag = true;\n' +
-    '    for (int j = 0; j < i; j++)\n' +
+    '    if (arr[j] > arr[j + 1])\n' +
     '    {\n' +
-    '      if (arr[j] > arr[j + 1])\n' +
-    '      {\n' +
-    '        swap(j, j + 1, arr);\n' +
-    '        flag = false;\n' +
-    '      }\n' +
-    '    }\n' +
-    '    if (flag)\n' +
-    '    {\n' +
-    '      break;\n' +
+    '      swap(j, j + 1, arr);\n' +
+    '      swapped = false;\n' +
     '    }\n' +
     '  }\n' +
-    '}',
-  C:
-    '  for (int i = length - 1; i >= 0; i--)\n' +
+    '  if (swapped)\n' +
     '  {\n' +
-    '    bool flag = true;\n' +
-    '    for (int j = 0; j < i; j++)\n' +
-    '    {\n' +
-    '      if (arr[j] > arr[j + 1])\n' +
-    '      {\n' +
-    '        swap(j, j + 1, arr);\n' +
-    '        flag = false;\n' +
-    '      }\n' +
-    '    }\n' +
-    '    if (flag)\n' +
-    '    {\n' +
-    '      break;\n' +
-    '    }\n' +
+    '    break;\n' +
     '  }\n' +
     '}',
 };
