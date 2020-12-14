e8dbc7232dc9ea8c88d01a615aac12d212abe99f
keanecjy <e0425601@u.nus.edu>
2020-12-14 23:54:57 +0800

    Add counting sort template

--- src/visualizer/codeinformation/templates/countingSortTemplate.js
+++ src/visualizer/codeinformation/templates/countingSortTemplate.js
@@ -1,128 +1,76 @@
 const CountingSort = {
   Java:
-    'void bucketSort(double[] arr) {\n' +
-    '    int totalSlots = 10;\n' +
-    '    ArrayList<ArrayList<Double>> buckets = new ArrayList<>(totalSlots);\n' +
+    '// Assume that given array only contains digits 0-9\n' +
+    'void countingSort(int[] arr) {\n' +
+    '    int[] countArr = new int[10];\n' +
     '\n' +
-    '    for (int i = 0; i < totalSlots; i++) {\n' +
-    '        ArrayList<Double> list = new ArrayList<>();\n' +
-    '        buckets.add(list);\n' +
-    '    }\n' +
-    '\n' +
-    '    for (double current : arr) {\n' +
-    '        int bucket_idx = (int) (totalSlots * current);\n' +
-    '        buckets.get(bucket_idx).add(current);\n' +
-    '    }\n' +
-    '\n' +
-    '    for (ArrayList<Double> bucket : buckets) {\n' +
-    '        Collections.sort(bucket);\n' +
+    '    for (int i = 0; i < arr.length; i++) {\n' +
+    '        countArr[arr[i]]++;\n' +
     '    }\n' +
     '\n' +
     '    int index = 0;\n' +
-    '    for (ArrayList<Double> bucket : buckets) {\n' +
-    '        for (double item : bucket) {\n' +
-    '            arr[index] = item;\n' +
+    '    for (int i = 0; i < countArr.length; i++) {\n' +
+    '        while (countArr[i] != 0) {\n' +
+    '            arr[index] = i;\n' +
     '            index++;\n' +
+    '            countArr[i]--;\n' +
     '        }\n' +
     '    }\n' +
-    '}\n\n',
+    '}\n',
   JavaScript:
-    'function bucketSort(arr) {\n' +
-    '  let buckets = [];\n' +
-    '  const totalSlots = 10;\n' +
-    '\n' +
-    '  for (let i = 0; i < totalSlots; i++) {\n' +
-    '    buckets[i] = [];\n' +
-    '  }\n' +
+    '// Assume that given array only contains digits 0-9\n' +
+    'function countingSort(arr) {\n' +
+    '  let countArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];\n' +
     '\n' +
     '  for (let i = 0; i < arr.length; i++) {\n' +
-    '    const current = arr[i];\n' +
-    '    const bucket_idx = Math.floor(totalSlots * current);\n' +
-    '    buckets[bucket_idx].push(current);\n' +
-    '  }\n' +
-    '\n' +
-    '  for (let i = 0; i < totalSlots; i++) {\n' +
-    '    buckets[i].sort();\n' +
+    '    countArr[arr[i]]++;\n' +
     '  }\n' +
     '\n' +
     '  let index = 0;\n' +
-    '  for (let i = 0; i < totalSlots; i++) {\n' +
-    '    for (let j = 0; j < buckets[i].length; j++) {\n' +
-    '      arr[index] = buckets[i][j];\n' +
-    '      index += 1;\n' +
+    '  for (let i = 0; i < countArr.length; i++) {\n' +
+    '    while (countArr[i] != 0) {\n' +
+    '      arr[index] = i;\n' +
+    '      index++;\n' +
+    '      countArr[i]--;\n' +
     '    }\n' +
     '  }\n' +
-    '}\n\n',
+    '}\n',
   Python:
-    'def bucket_sort(arr):\n' +
-    '    buckets = []\n' +
-    '    total_slots = 10\n' +
+    '# Assume that given array only contains digits 0-9\n' +
+    'def counting_sort(arr):\n' +
+    '    count_arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]\n' +
     '\n' +
-    '    for i in range(0, total_slots):\n' +
-    '        buckets.append([])\n' +
-    '\n' +
-    '    for item in arr:\n' +
-    '        bucket_idx = int(total_slots * item)\n' +
-    '        buckets[bucket_idx].append(item)\n' +
-    '\n' +
-    '    for bucket in buckets:\n' +
-    '        bucket = sorted(bucket)\n' +
+    '    for i in range(0, len(arr)):\n' +
+    '        count_arr[arr[i]] += 1\n' +
     '\n' +
     '    index = 0\n' +
-    '    for bucket in buckets:\n' +
-    '        for item in bucket:\n' +
-    '            arr[index] = item\n' +
-    '            index += 1\n\n',
+    '    for i in range(0, len(count_arr)):\n' +
+    '        while count_arr[i] is not 0:\n' +
+    '            arr[index] = i\n' +
+    '            index += 1\n' +
+    '            count_arr[i] -= 1\n',
   'C/C++':
-    'struct Node\n' +
+    '// Assume that given array only contains digits 0-9\n' +
+    'void countingSort(int arr[], int len)\n' +
     '{\n' +
-    '    int data;\n' +
-    '    struct Node *next;\n' +
-    '};\n' +
+    '    int countArr[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};\n' +
     '\n' +
-    'void bucketSort(double arr[], int len)\n' +
-    '{\n' +
-    '    int i, j;\n' +
-    '    struct Node **buckets;\n' +
-    '    int totalSlots = 10;\n' +
-    '    buckets = (struct Node **)malloc(sizeof(struct Node *) * len);\n' +
-    '\n' +
-    '    for (i = 0; i < totalSlots; ++i)\n' +
+    '    for (int i = 0; i < len; i++)\n' +
     '    {\n' +
-    '        buckets[i] = NULL;\n' +
+    '        countArr[arr[i]]++;\n' +
     '    }\n' +
     '\n' +
-    '    for (i = 0; i < len; ++i)\n' +
+    '    int index = 0;\n' +
+    '    for (int i = 0; i < 10; i++)\n' +
     '    {\n' +
-    '        struct Node *current;\n' +
-    '        int pos = (int)(arr[i] * totalSlots);\n' +
-    '        current = (struct Node *)malloc(sizeof(struct Node));\n' +
-    '        current->data = arr[i];\n' +
-    '        current->next = buckets[pos];\n' +
-    '        buckets[pos] = current;\n' +
-    '    }\n' +
-    '\n' +
-    '    for (i = 0; i < totalSlots; ++i)\n' +
-    '    {\n' +
-    '        sort(buckets[i]);\n' +
-    '    }\n' +
-    '\n' +
-    '    for (j = 0, i = 0; i < totalSlots; ++i)\n' +
-    '    {\n' +
-    '        struct Node *node;\n' +
-    '        node = buckets[i];\n' +
-    '        while (node)\n' +
+    '        while (countArr[i] != 0)\n' +
     '        {\n' +
-    '            arr[j++] = node->data;\n' +
-    '            node = node->next;\n' +
+    '            arr[index] = i;\n' +
+    '            index++;\n' +
+    '            countArr[i]--;\n' +
     '        }\n' +
     '    }\n' +
-    '}\n' +
-    '\n' +
-    'void bucketSortMain(double arr[], int len)\n' +
-    '{\n' +
-    '    bucketSort(arr, len);\n' +
-    '}\n\n',
+    '}\n',
 };
 
 export default CountingSort;
