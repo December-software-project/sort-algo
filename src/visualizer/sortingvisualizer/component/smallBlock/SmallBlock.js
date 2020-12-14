0278e6a978bb65f881c98305454be9805937f072
keanecjy <e0425601@u.nus.edu>
2020-12-15 00:26:20 +0800

    Remove unused object

--- src/visualizer/sortingvisualizer/component/smallBlock/SmallBlock.js
+++ src/visualizer/sortingvisualizer/component/smallBlock/SmallBlock.js
@@ -2,18 +2,6 @@ import React from 'react';
 import '../block/styles.css';
 import { animated } from 'react-spring';
 
-export const heightMapping = {
-  1: 19,
-  2: 19,
-  3: 19,
-  4: 19,
-  5: 19,
-  6: 19,
-  7: 19,
-  8: 19,
-  9: 19,
-}
-
 const SmallBlock = ({ item, props: { x, ...rest }, length, index, width }) => {
   return (
     <animated.div
