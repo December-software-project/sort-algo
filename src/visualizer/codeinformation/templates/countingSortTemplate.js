be62f68eab4582029fddbb9d3e303e039de77300
keanecjy <e0425601@u.nus.edu>
2020-12-15 14:03:02 +0800

    Update counting sort template

--- src/visualizer/codeinformation/templates/countingSortTemplate.js
+++ src/visualizer/codeinformation/templates/countingSortTemplate.js
@@ -1,13 +1,23 @@
 const CountingSort = {
   Java:
-    '// Assume given array only contains digits 0-9\n' +
+    '// Assume array contains only non-zero digits\n' +
     'void countingSort(int[] arr) {\n' +
-    '    int[] countArr = new int[10];\n' +
     '\n' +
+    '    int max = arr[0];\n' +
+    '    // Get max element\n' +
+    '    for (int i = 1; i < arr.length; i++) {\n' +
+    '        max = Math.max(max, arr[i]);\n' +
+    '    }\n' +
+    '\n' +
+    '    // Create count array containing of size = max + 1\n' +
+    '    int[] countArr = new int[max + 1];\n' +
+    '\n' +
+    '    // Fill in count arr\n' +
     '    for (int i = 0; i < arr.length; i++) {\n' +
     '        countArr[arr[i]]++;\n' +
     '    }\n' +
     '\n' +
+    '    // Insert elements back into original array\n' +
     '    int index = 0;\n' +
     '    for (int i = 0; i < countArr.length; i++) {\n' +
     '        while (countArr[i] != 0) {\n' +
@@ -18,14 +28,21 @@ const CountingSort = {
     '    }\n' +
     '}\n',
   JavaScript:
-    '// Assume given array only contains digits 0-9\n' +
+    '// Assume array contains only non-zero digits\n' +
     'function countingSort(arr) {\n' +
-    '  let countArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];\n' +
+    '  // Get max element\n' +
+    '  const maxElement = Math.max(...arr);\n' +
     '\n' +
+    '  // Create count array containing of size = max + 1\n' +
+    '  const countArr = new Array(maxElement + 1);\n' +
+    '  countArr.fill(0);\n' +
+    '\n' +
+    '  // Fill in count arr\n' +
     '  for (let i = 0; i < arr.length; i++) {\n' +
     '    countArr[arr[i]]++;\n' +
     '  }\n' +
     '\n' +
+    '  // Insert elements back into original array\n' +
     '  let index = 0;\n' +
     '  for (let i = 0; i < countArr.length; i++) {\n' +
     '    while (countArr[i] != 0) {\n' +
@@ -36,32 +53,59 @@ const CountingSort = {
     '  }\n' +
     '}\n',
   Python:
-    '# Assume given array only contains digits 0-9\n' +
+    '# Assume array contains only non-zero digits\n' +
     'def counting_sort(arr):\n' +
-    '    count_arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]\n' +
+    '    # Get max element\n' +
+    '    maxElement = max(arr)\n' +
+    '    print(maxElement)\n' +
     '\n' +
-    '    for i in range(0, len(arr)):\n' +
+    '    # Create count array containing of size = max + 1\n' +
+    '    count_arr = [0 for i in range(maxElement + 1)]\n' +
+    '\n' +
+    '    # Fill in count arr\n' +
+    '    for i in range(len(arr)):\n' +
     '        count_arr[arr[i]] += 1\n' +
     '\n' +
+    '    # Insert elements back into original array\n' +
     '    index = 0\n' +
-    '    for i in range(0, len(count_arr)):\n' +
+    '    for i in range(len(count_arr)):\n' +
     '        while count_arr[i] is not 0:\n' +
     '            arr[index] = i\n' +
     '            index += 1\n' +
     '            count_arr[i] -= 1\n',
   'C/C++':
-    '// Assume given array only contains digits 0-9\n' +
+    '// Assume array contains only non-zero digits\n' +
     'void countingSort(int arr[], int len)\n' +
     '{\n' +
-    '    int countArr[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};\n' +
     '\n' +
+    '    int maxElement = arr[0];\n' +
+    '    // Get max element\n' +
+    '    for (int i = 1; i < len; i++)\n' +
+    '    {\n' +
+    '        if (arr[i] > maxElement)\n' +
+    '        {\n' +
+    '            maxElement = arr[i];\n' +
+    '        }\n' +
+    '    }\n' +
+    '\n' +
+    '    // Create array with length maxElement + 1\n' +
+    '    int countArrLength = maxElement + 1;\n' +
+    '    int *countArr = (int *)malloc(countArrLength * sizeof(int));\n' +
+    '\n' +
+    '    for (int i = 0; i < countArrLength; i++)\n' +
+    '    {\n' +
+    '        countArr[i] = 0;\n' +
+    '    }\n' +
+    '\n' +
+    '    // Fill in count arr\n' +
     '    for (int i = 0; i < len; i++)\n' +
     '    {\n' +
     '        countArr[arr[i]]++;\n' +
     '    }\n' +
     '\n' +
+    '    // Insert elements back into original array\n' +
     '    int index = 0;\n' +
-    '    for (int i = 0; i < 10; i++)\n' +
+    '    for (int i = 0; i < countArrLength; i++)\n' +
     '    {\n' +
     '        while (countArr[i] != 0)\n' +
     '        {\n' +
@@ -70,7 +114,7 @@ const CountingSort = {
     '            countArr[i]--;\n' +
     '        }\n' +
     '    }\n' +
-    '}\n',
+    '}\n\n',
 };
 
 export default CountingSort;
