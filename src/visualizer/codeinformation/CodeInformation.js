14260c69ed54ed9b1cf9bff69a8c8579f9d9aa14
keanecjy <e0425601@u.nus.edu>
2020-12-26 16:59:13 +0800

    Add jsdoc for code explanation component

--- src/visualizer/codeinformation/CodeInformation.js
+++ src/visualizer/codeinformation/CodeInformation.js
@@ -3,6 +3,13 @@ import CodeTemplate from './codetemplate/CodeTemplate';
 import React from 'react';
 import './styles.css';
 
+/**
+ * Code information component which provides information on the current sorting algorithm.
+ *
+ * @component
+ * @param {string} visualizerAlgorithm Current sorting algorithm selected.
+ * @returns {JSX.Element} Code information component which provides information on the current sorting algorithm.
+ */
 const CodeInformation = ({ visualizerAlgorithm }) => (
   <div className="code">
     <CodeExplanation algo={visualizerAlgorithm} />
