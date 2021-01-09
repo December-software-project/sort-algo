8356650ab310179ecbeb4e9a6d17b961109a417f
keanecjy <e0425601@u.nus.edu>
2021-01-09 17:14:00 +0800

    Update links

--- src/team/profile/Profile.js
+++ src/team/profile/Profile.js
@@ -10,10 +10,16 @@ const Profile = ({ img, name, description, linkedInLink, gitHubLink }) => {
       <div className="profile-description-container">
         <p className="profile-title">{name}</p>
         <p className="profile-description">{description}</p>
-        <a href={linkedInLink} target="_blank" rel="noopener noreferrer">
+        <a title="LinkedIn" href={linkedInLink} target="_blank" rel="noopener noreferrer">
           <FaLinkedin size={23} color={'#0e76a8'} />
         </a>
-        <a href={gitHubLink} target="_blank" rel="noopener noreferrer" className="github-logo">
+        <a
+          title="GitHub"
+          href={gitHubLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="github-logo"
+        >
           <FaGithub size={23} color={'#211F1F'} />
         </a>
       </div>
